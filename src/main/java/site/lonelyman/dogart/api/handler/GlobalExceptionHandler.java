e0f2a937a90f384bf536574267c79c08c93cc643
LM <me@lonelyman.site>
2022-12-08 09:51:45 +0800

    feat:新增全局异常处理

---
+++ src/main/java/site/lonelyman/dogart/api/handler/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package site.lonelyman.dogart.api.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import site.lonelyman.dogart.api.model.Result;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author LM
+ * @since 2022/12/8
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public Result<Object> runtimeExceptionHandler(RuntimeException e) {
+        return Result.error(e.getMessage());
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public Result<Object> exceptionHandler(Exception e) {
+        return Result.error("未知异常");
+    }
+
+}
